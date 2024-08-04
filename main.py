2cce897744a00dd85670f2fb691f28936983f992
Filipe Alano Fortunato <62904031+filipealft@users.noreply.github.com>
2024-08-03 22:42:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from dataframe_builder import DataFrameBuilder
 from csv_exporter import CSVExporter
 
 def main():
+    """
+    Main function to execute the GitHub followers data collection and processing.
+    """
     github_token = os.getenv('GITHUB_TOKEN')
     user = os.getenv('USER')
     csv_output = f"output/github_{user}_followers"
 
     if not github_token or not user:
-        raise ValueError("As variáveis de ambiente GITHUB_TOKEN e USER devem ser definidas.")
+        raise ValueError("Environment variables GITHUB_TOKEN and USER must be set.")
 
     github_client = GitHubClient(github_token, user)
     dataframe_builder = DataFrameBuilder()
@@ -20,7 +23,7 @@ def main():
         dataframe = dataframe_builder.process_followers_data(followers_data)
         csv_exporter.save_to_csv(dataframe)
     else:
-        print("Nenhum dado de seguidores encontrado.")
+        print("No follower data found.")
 
 if __name__ == "__main__":
     main()
